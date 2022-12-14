10498c8aa5f97f43d6ffd39fe4fb1f5268465eb8
root <softtech.series@gmail.com>
2022-12-14 00:01:54 -0800

    first task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+# include <stdio.h>
+#include <time.h>
+
+/:
+main  -main function to generate arandom number
+.
+ruturn always 0 (success)
+.
+./
+int main(void)
+{
+	int n;
+	srand(time(0)):
+	
+		srand(time(0));
+
+		n = rand() - RAND_MAX / 2;
+
+			/* your code goes there */
+
+			return (0);
+
+}
